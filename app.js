28fa0d22831ca9fbf04c642ed58f016785e2c037
Drago78624 <maazahmed78624@gmail.com>
2022-08-14 12:33:31 +0500

    divide by 0 bug to be fixed

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const equal = document.querySelector(".equal");
 const clear = document.querySelector(".clear");
 const display = document.querySelector(".display");
 
-let num1 = 0;
-let num2 = 0;
+let num1 = "";
+let num2 = "";
 let operator = "";
 
 function operate(num1, num2, operator){
@@ -22,9 +22,16 @@ function operate(num1, num2, operator){
             display.innerText = num1 * num2
             break;
         case "/":
-            display.innerText = num1 / num2
+            if(num2 === 0){
+                display.innerText = "GET LOST"
+            }else {
+                display.innerText = num1 / num2
+            }
             break;
     }
+    console.log(num1, operator, num2)
+
+
 }
 
 numbers.forEach(number => {
@@ -37,38 +44,42 @@ numbers.forEach(number => {
             }
             num1 = parseInt(display.innerText)
         }else {
-            if(num2 === 0){
+            if(num2 === ""){
                 display.innerText = number.innerText;
             }else {
                 display.innerText += number.innerText;
             }
             num2 = parseInt(display.innerText)
         }
-        console.log(num1, num2, operator)
-
+        console.log(num1, operator, num2)
     })
 });
 
 operators.forEach(operatorBtn => {
     operatorBtn.addEventListener("click", ()=>{
-        operator = operatorBtn.innerText
-        if(num1 && num2 && operator){
+        if(num1 != "" && num2 != "" || num2 == 0 && operator != ""){
             operate(num1, num2, operator)
+            num1 = parseInt(display.innerText)
+            num2 = "";
         }
+        operator = operatorBtn.innerText
+        console.log(num1, operator, num2)
     })
 });
 
 equal.addEventListener("click", ()=>{
-    if(num1 && num2 && operator){
+    if(num1 != "" && num2 != "" && operator != ""){
         operate(num1, num2, operator)
+        num1 = parseInt(display.innerText)
+        num2 = "";
     }
+    console.log(num1, operator, num2)
 })
 
 clear.addEventListener("click", ()=>{
-    display.innerText = 0;
-    num1 = 0;
-    num2 = 0;
+    display.innerText = "0";
+    num1 = "";
+    num2 = "";
     operator = "";
-console.log(num1, num2, operator)
-
+    console.log(num1, operator, num2)
 })
